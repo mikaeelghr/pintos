3d49dc09361b6d5fca06e416fc0fc7505d6828dd
Mikaeel Ghorbani <mikaeelghr@gmail.com>
2021-03-15 18:58:02 +0000

    add pointer validator on syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,6 +6,8 @@
 #include "threads/thread.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
+#include "threads/vaddr.h"
+
 
 #define put_error_on_frame_when_null(o, f) if (o == NULL) f->eax=-1
 #define return_on_null(o) if (o == NULL) return
@@ -42,11 +44,38 @@ get_file_from_fd (struct list *l, int the_fd)
   return file_descriptor_instance->file;
 }
 
+static bool is_char_pointer_valid(const char* ptr){
+	char* endptr=ptr;
+	thread* t=&thread_current();
+	if(ptr<t){
+		return 0;
+	}
+	while(endptr<t+PGSIZE && *endptr!=0){
+		endptr++;
+	}
+	if(endptr==t+PGSIZE){
+		return 0;
+	}
+	return 1;
+}
+
+static bool is_pointer_valid(const void* ptr, const uint32_t size=sizeof(uint32_t)){
+	thread* t=&thread_current();
+	if(ptr<t || ptr+size>t+PGSIZE){
+		return 0;
+	}
+	return 1;
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
   uint32_t *args = ((uint32_t *) f->esp);
-
+  if(!is_pointer_valid(args) || !is_pointer_valid(args+1)){
+    f->eax=-1;
+    return;
+  }
+  
   /*
    * The following print statement, if uncommented, will print out the syscall
    * number whenever a process enters a system call. You might find it useful
@@ -61,9 +90,13 @@ syscall_handler (struct intr_frame *f UNUSED)
       f->eax = args[1];
       printf ("%s: exit(%d)\n", &thread_current ()->name, args[1]);
       thread_exit ();
-    }
+    }i
   else if (args[0] == SYS_OPEN)
     {
+      if(is_char_pointer_valid(args[1])){
+	f->eax=-1;
+	return;
+      }
       struct file *fi = filesys_open (args[1]);
       put_error_on_frame_when_null(fi, f);
       return_on_null(fi);
@@ -83,6 +116,10 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
   else if (args[0] == SYS_WRITE)
     {
+      if (!is_pointer_valid(args+2) || !is_pointer_valid(args+3) || !is_char_pointer_valid(args[2])){
+	f->eax=-1;
+	return;
+      }
       if (args[1] == STDOUT_FILENO)
         {
           const char *buffer = (char *) args[2];
@@ -102,6 +139,10 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
   else if (args[0] == SYS_READ)
     {
+      if (!is_pointer_valid(args+2) || !is_pointer_valid(args+3) || !is_char_pointer_valid(args[2])){
+      	f->eax=-1;
+	return;
+      }
       struct file *fi = get_file_from_fd (&thread_current ()->file_descriptors, args[1]);
       put_error_on_frame_when_null(fi, f);
       return_on_null(fi);
@@ -109,10 +150,15 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
   else if (args[0] == SYS_CREATE)
     {
+      if (!is_pointer_valid(args+2) || !is_char_pointer_valid(args[1])){
+      	f->eax=-1;
+	return;
+      }
       f->eax = filesys_create (args[1], args[2]);
     }
   else if (args[0] == SYS_CLOSE)
     {
+      
       struct file_descriptor *file_descriptor_instance = get_file_descriptor_from_fd (
               &thread_current ()->file_descriptors, args[1]);
       return_on_null(file_descriptor_instance);
@@ -122,6 +168,10 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
   else if (args[0] == SYS_SEEK)
     {
+      if (!is_pointer_valid(args+2)){
+      	f->eax=-1;
+	return;
+      }
       struct file *fi = get_file_from_fd (&thread_current ()->file_descriptors, args[1]);
       return_on_null(fi);
       file_seek (fi, args[2]);
@@ -135,6 +185,10 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
   else if (args[0] == SYS_REMOVE)
     {
+      if (!is_char_pointer_valid(args[1])){
+      	f->eax=-1;
+	return;
+      }
       f->eax = filesys_remove (args[1]);
     }
 }
